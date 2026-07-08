11c19b76d5976d6542e693ca1796ab683ae6cf2d
VINSMOKE7-SANJI <alfaberkat09@gmail.com>
2026-07-09 00:55:36 +0700

    Fix: Perbaiki musik dan countdown - hapus toggleMusic error, perbaiki perhitungan jam di countdown

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ const countdownInterval = setInterval(() => {
     }
 
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
